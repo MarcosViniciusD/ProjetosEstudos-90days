7f389b90e51bea765037f1eb774b6738a9932314
MarcosviniciusD <Marcos.duarte1501@gmail.com>
2024-11-05 14:47:43 -0300

    Conceito de como funciona EventLoop

---
+++ Dia10-EventLoop/index.js
@@ -0,0 +1,24 @@
+/**
+ * É um recurso da arquitetura do node.
+ * Considerações: Tomar cuidado para nao criar loops infinitos e acabar derrubando nossa aplicação quando após o deploy;
+ * 
+ *O codigo é lido de cima para baixo de forma sequencial.
+ */
+
+function primeiraFuncao(){
+    console.log('executando a primeira função')
+}
+function segundaFuncao(){
+    console.log('executando a segunda função')
+}
+function terceiraFuncao(){
+    console.log('executando a terceira função')
+    primeiraFuncao();
+segundaFuncao();
+
+}
+
+
+terceiraFuncao();
+
+//Com isso, sabemos que o event Loop garante que o codigo execute de forma SEQUENCIAL!!!
